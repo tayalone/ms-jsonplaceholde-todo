123bb1b4bfb8c093d45dd3f73128ab3e6fd26638
Panupong Rong-O <tayalone@gmail.com>
2022-10-09 15:50:25 +0700

    ✨ Create "ToDo" Struct

--- core/domain/domain.go
+++ core/domain/domain.go
@@ -1,7 +1,13 @@
 package domain
 
-import "fmt"
+import "time"
 
-func domain() {
-	fmt.Println("waiting to create Domain")
+/*ToDo Represent Domain Problem Data */
+type ToDo struct {
+	ID        uint      `json:"id"`
+	UserID    uint      `json:"userId:"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdateAt  time.Time `json:"updatedAt"`
 }
