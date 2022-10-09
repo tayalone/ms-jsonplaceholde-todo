784e2837f8b1e1b7aaeab2da6d46447b393948c9
Panupong Rong-O <tayalone@gmail.com>
2022-10-09 16:06:30 +0700

    ✨ create "Port" interface

--- core/port/port.go
+++ core/port/port.go
@@ -1,7 +1,17 @@
 package port
 
-import "fmt"
+import "github.com/tayalone/ms-jsonplaceholde-todo/core/domain"
 
-func port() {
-	fmt.Println("Waiting For Create Port")
+/*ToDoRpstr define Bahavior of ToDo Repository */
+type ToDoRpstr interface {
+	Create(userID uint, title string) domain.ToDo
+	UpdateByPk(id uint, title string, completed bool) (domain.ToDo, error)
+	DeleteByPk(id uint) error
+}
+
+/*ToDoSrvc define Bahavior of ToDo Services */
+type ToDoSrvc interface {
+	New(userID uint, title string) domain.ToDo
+	UpdateByID() (domain.ToDo, error)
+	DeleteByID(id uint) error
 }
